56dd5074ceef3e6d97e044936782059522b1f8b4
Berkay Yiğit <berkaytgy@gmail.com>
2020-05-31 16:58:08 +0300

    Add namespace to MW05/Game.h

--- OpenSpeed/Game.MW05/Types/Game.h
+++ OpenSpeed/Game.MW05/Types/Game.h
@@ -20,31 +20,40 @@
 #pragma once
 #include <OpenSpeed/Game.MW05/Types.h>
 
-namespace OpenSpeed::MW05 {
+namespace OpenSpeed::MW05::Game {
   static inline auto BeginGameFlowUnloadTrack() { reinterpret_cast<void(__cdecl*)()>(0x667340)(); }
-  static inline void ECameraShake() { reinterpret_cast<void(__cdecl*)()>(0x62B110)(); }
-  static inline void EForcePursuitBail() { reinterpret_cast<void(__cdecl*)()>(0x62ACE0)(); }
-  // Also sets the heat level to max
-  static inline void EForcePursuitStart() { reinterpret_cast<void(__cdecl*)()>(0x60AAC0)(); }
-  static inline void Game_AbandonRace() { reinterpret_cast<void(__cdecl*)()>(0x60DEB0)(); }
-  static inline void Game_BlowEngine(ISimable* pTarget) {
-    reinterpret_cast<void(__cdecl*)(ISimable*)>(0x60AB30)(pTarget);
-  }
-  static inline void Game_ClearAIControl(bool _unk = true) { reinterpret_cast<void(__cdecl*)(bool)>(0x612420)(_unk); }
-  static inline void Game_ForceAIControl(bool _unk = true) { reinterpret_cast<void(__cdecl*)(bool)>(0x6123B0)(_unk); }
-  static inline void Game_JumpToCarLot() { reinterpret_cast<void(__cdecl*)()>(0x605250)(); }
-  static inline void Game_JumpToSafehouse() { reinterpret_cast<void(__cdecl*)()>(0x6052B0)(); }
-  static inline void Game_SabotageEngine(ISimable* pTarget, float durationInSeconds) {
+  // ECameraShake
+  static inline void ShakeCamera() { reinterpret_cast<void(__cdecl*)()>(0x62B110)(); }
+  // EForcePursuitBail
+  static inline void ForcePursuitBail() { reinterpret_cast<void(__cdecl*)()>(0x62ACE0)(); }
+  // EForcePursuitStart, also sets the heat level to max
+  static inline void ForcePursuitStart() { reinterpret_cast<void(__cdecl*)()>(0x60AAC0)(); }
+  // Game_AbandonRace
+  static inline void AbandonRace() { reinterpret_cast<void(__cdecl*)()>(0x60DEB0)(); }
+  // Game_BlowEngine
+  static inline void BlowEngine(ISimable* pTarget) { reinterpret_cast<void(__cdecl*)(ISimable*)>(0x60AB30)(pTarget); }
+  // Game_ClearAIControl
+  static inline void ClearAIControl(bool _unk = true) { reinterpret_cast<void(__cdecl*)(bool)>(0x612420)(_unk); }
+  // Game_ForceAIControl
+  static inline void ForceAIControl(bool _unk = true) { reinterpret_cast<void(__cdecl*)(bool)>(0x6123B0)(_unk); }
+  // Game_JumpToCarLot
+  static inline void JumpToCarLot() { reinterpret_cast<void(__cdecl*)()>(0x605250)(); }
+  // Game_JumpToSafehouse
+  static inline void JumpToSafehouse() { reinterpret_cast<void(__cdecl*)()>(0x6052B0)(); }
+  // Game_SabotageEngine
+  static inline void SabotageEngine(ISimable* pTarget, float durationInSeconds) {
     reinterpret_cast<void(__cdecl*)(ISimable*, float)>(0x60AB60)(pTarget, durationInSeconds);
   }
-  static inline void Game_SetCopsEnabled(bool areCopsEnabled) {
-    reinterpret_cast<void(__cdecl*)(bool)>(0x604F40)(areCopsEnabled);
-  }
-  static inline void Game_SetWorldHeat(float newHeatLevel) {
+  // Game_SetCopsEnabled
+  static inline void SetCopsEnabled(bool copsEnabled) { reinterpret_cast<void(__cdecl*)(bool)>(0x604F40)(copsEnabled); }
+  // Game_SetWorldHeat
+  static inline void SetWorldHeat(float newHeatLevel) {
     reinterpret_cast<void(__cdecl*)(float)>(0x612660)(newHeatLevel);
   }
-  static inline void Game_ShowPauseMenu() { reinterpret_cast<void(__cdecl*)()>(0x6050F0)(); }
+  // Game_ShowPauseMenu
+  static inline void ShowPauseMenu() { reinterpret_cast<void(__cdecl*)()>(0x6050F0)(); }
+
   static inline auto stringhash32(const char* cstring) {
     return reinterpret_cast<std::uint32_t(__cdecl*)(const char*)>(0x5CC240)(cstring);
   }
-}  // namespace OpenSpeed::MW05
+}  // namespace OpenSpeed::MW05::Game
