19c04319b1fbecce901fa92a1c26428aeb2bf4c0
Berkay YİĞİT <berkayyigit@anadolu.edu.tr>
2022-03-26 17:40:19 +0300

    [MW05] Add AIVehicleEx

--- OpenSpeed/Game.MW05/Extensions.h
+++ OpenSpeed/Game.MW05/Extensions.h
@@ -21,11 +21,16 @@
 #include <functional>  // std::function
 
 #include <OpenSpeed/Game.MW05/Types.h>
-#include <OpenSpeed/Game.MW05/Types/GRaceStatus.h>  // GRaceStatus
-#include <OpenSpeed/Game.MW05/Types/InputPlayer.h>  // InputPlayer, PInput, IInput
-#include <OpenSpeed/Game.MW05/Types/IPlayer.h>      // IPlayer
-#include <OpenSpeed/Game.MW05/Types/PVehicle.h>     // PVehicle
-#include <OpenSpeed/Game.MW05/Types/RBTractor.h>    // RBTractor, RBVehicle, RigidBody, IRigidBody
+#include <OpenSpeed/Game.MW05/Types/AIVehicleCopCar.h>  // AIVehicleCopCar, AIVehiclePursuit, AIVehiclePid, AIVehicle
+#include <OpenSpeed/Game.MW05/Types/AIVehicleEmpty.h>   // AIVehicleEmpty
+#include <OpenSpeed/Game.MW05/Types/AIVehicleHelicopter.h>  // AIVehicleHelicopter
+#include <OpenSpeed/Game.MW05/Types/AIVehicleHuman.h>       // AIVehicleHuman, AIVehicleRacecar
+#include <OpenSpeed/Game.MW05/Types/AIVehicleTraffic.h>     // AIVehicleTraffic
+#include <OpenSpeed/Game.MW05/Types/GRaceStatus.h>          // GRaceStatus
+#include <OpenSpeed/Game.MW05/Types/InputPlayer.h>          // InputPlayer, PInput, IInput
+#include <OpenSpeed/Game.MW05/Types/IPlayer.h>              // IPlayer
+#include <OpenSpeed/Game.MW05/Types/PVehicle.h>             // PVehicle
+#include <OpenSpeed/Game.MW05/Types/RBTractor.h>            // RBTractor, RBVehicle, RigidBody, IRigidBody
 
 namespace OpenSpeed::MW05 {
   //          //
@@ -153,48 +158,48 @@ namespace OpenSpeed::MW05 {
       //                         //
 
       struct RigidBodyCast_t {
-        RigidBody* operator()(IRigidBody* irb) const {
-          if (!irb) return nullptr;
+        RigidBody* operator()(IRigidBody* iRB) const {
+          if (!iRB) return nullptr;
           // Verify cast
-          auto* rb = static_cast<RigidBody*>(irb);
+          auto* rb = static_cast<RigidBody*>(iRB);
           if (*reinterpret_cast<std::uintptr_t*>(rb) == static_cast<std::uintptr_t>(0x8AC448)) return rb;
           // Bad cast
           return nullptr;
         }
       };
-      static RigidBody* operator|(IRigidBody* rb, RigidBodyCast_t ext) { return ext(rb); }
+      static RigidBody* operator|(IRigidBody* iRB, RigidBodyCast_t ext) { return ext(iRB); }
 
       //                         //
       // Verified RBVehicle cast //
       //                         //
 
       struct RBVehicleCast_t {
-        RBVehicle* operator()(IRigidBody* irb) const {
-          if (!irb) return nullptr;
+        RBVehicle* operator()(IRigidBody* iRB) const {
+          if (!iRB) return nullptr;
           // Verify cast
-          auto* rb = static_cast<RBVehicle*>(irb);
+          auto* rb = static_cast<RBVehicle*>(iRB);
           if (*reinterpret_cast<std::uintptr_t*>(rb) == static_cast<std::uintptr_t>(0x8AC938)) return rb;
           // Bad cast
           return nullptr;
         }
       };
-      static RBVehicle* operator|(IRigidBody* rb, RBVehicleCast_t ext) { return ext(rb); }
+      static RBVehicle* operator|(IRigidBody* iRB, RBVehicleCast_t ext) { return ext(iRB); }
 
       //                         //
       // Verified RBTractor cast //
       //                         //
 
       struct RBTractorCast_t {
-        RBTractor* operator()(IRigidBody* irb) const {
-          if (!irb) return nullptr;
+        RBTractor* operator()(IRigidBody* iRB) const {
+          if (!iRB) return nullptr;
           // Verify cast
-          auto* rb = static_cast<RBTractor*>(irb);
+          auto* rb = static_cast<RBTractor*>(iRB);
           if (*reinterpret_cast<std::uintptr_t*>(rb) == static_cast<std::uintptr_t>(0x8ACBA8)) return rb;
           // Bad cast
           return nullptr;
         }
       };
-      static RBTractor* operator|(IRigidBody* rb, RBTractorCast_t ext) { return ext(rb); }
+      static RBTractor* operator|(IRigidBody* iRB, RBTractorCast_t ext) { return ext(iRB); }
     }  // namespace details
 
     // Try-get IRigidBody as RigidBody
@@ -228,33 +233,33 @@ namespace OpenSpeed::MW05 {
       // Verified PInput cast //
       //                      //
 
-      struct PInputCast_t {
-        PInput* operator()(IInput* i) const {
-          if (!i) return nullptr;
+      struct PInputCast_t { 
+        PInput* operator()(IInput* iInput) const {
+          if (!iInput) return nullptr;
           // Verify cast
-          auto* pi = static_cast<PInput*>(i);
+          auto* pi = static_cast<PInput*>(iInput);
           if (*reinterpret_cast<std::uintptr_t*>(pi) == static_cast<std::uintptr_t>(0x8AB598)) return pi;
           // Bad cast
           return nullptr;
         }
       };
-      static PInput* operator|(IInput* i, PInputCast_t ext) { return ext(i); }
+      static PInput* operator|(IInput* iInput, PInputCast_t ext) { return ext(iInput); }
 
       //                           //
       // Verified InputPlayer cast //
       //                           //
 
       struct InputPlayerCast_t {
-        InputPlayer* operator()(IInput* i) const {
-          if (!i) return nullptr;
+        InputPlayer* operator()(IInput* iInput) const {
+          if (!iInput) return nullptr;
           // Verify cast
-          auto* ip = static_cast<InputPlayer*>(i);
+          auto* ip = static_cast<InputPlayer*>(iInput);
           if (*reinterpret_cast<std::uintptr_t*>(ip) == static_cast<std::uintptr_t>(0x8AC6BC)) return ip;
           // Bad cast
           return nullptr;
         }
       };
-      static InputPlayer* operator|(IInput* i, InputPlayerCast_t ext) { return ext(i); }
+      static InputPlayer* operator|(IInput* iInput, InputPlayerCast_t ext) { return ext(iInput); }
     }  // namespace details
 
     // Try-get IInput as PInput
@@ -273,4 +278,181 @@ namespace OpenSpeed::MW05 {
       return nullptr;
     }
   }  // namespace InputEx
+
+  //            //
+  // IVehicleAI //
+  //            //
+
+  namespace AIVehicleEx {
+    namespace details {
+      //                         //
+      // Verified AIVehicle cast //
+      //                         //
+
+      struct AIVehicleCast_t {
+        AIVehicle* operator()(IVehicleAI* iAI) const {
+          if (!iAI) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehicle*>(iAI);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x891A80)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehicle* operator|(IVehicleAI* i, AIVehicleCast_t ext) { return ext(i); }
+
+      //                               //
+      // Verified AIVehicleCopCar cast //
+      //                               //
+
+      struct AIVehicleCopCarCast_t {
+        AIVehicleCopCar* operator()(IVehicleAI* iAI) const {
+          if (!iAI) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehicleCopCar*>(iAI);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x892560)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehicleCopCar* operator|(IVehicleAI* i, AIVehicleCopCarCast_t ext) { return ext(i); }
+
+      //                              //
+      // Verified AIVehicleEmpty cast //
+      //                              //
+
+      struct AIVehicleEmptyCast_t {
+        AIVehicleEmpty* operator()(IVehicleAI* i) const {
+          if (!i) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehicleEmpty*>(i);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x892E28)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehicleEmpty* operator|(IVehicleAI* iAI, AIVehicleEmptyCast_t ext) { return ext(iAI); }
+
+      //                                   //
+      // Verified AIVehicleHelicopter cast //
+      //                                   //
+
+      struct AIVehicleHelicopterCast_t {
+        AIVehicleHelicopter* operator()(IVehicleAI* i) const {
+          if (!i) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehicleHelicopter*>(i);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x8920D8)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehicleHelicopter* operator|(IVehicleAI* i, AIVehicleHelicopterCast_t ext) { return ext(i); }
+
+      //                              //
+      // Verified AIVehicleHuman cast //
+      //                              //
+
+      struct AIVehicleHumanCast_t {
+        AIVehicleHuman* operator()(IVehicleAI* iAI) const {
+          if (!iAI) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehicleHuman*>(iAI);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x892AD0)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehicleHuman* operator|(IVehicleAI* i, AIVehicleHumanCast_t ext) { return ext(i); }
+
+      //                            //
+      // Verified AIVehiclePid cast //
+      //                            //
+
+      struct AIVehiclePidCast_t {
+        AIVehiclePid* operator()(IVehicleAI* iAI) const {
+          if (!iAI) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehiclePid*>(iAI);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x891BB8)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehiclePid* operator|(IVehicleAI* i, AIVehiclePidCast_t ext) { return ext(i); }
+
+      //                                //
+      // Verified AIVehiclePursuit cast //
+      //                                //
+
+      struct AIVehiclePursuitCast_t {
+        AIVehiclePursuit* operator()(IVehicleAI* iAI) const {
+          if (!iAI) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehiclePursuit*>(iAI);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x891EC0)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehiclePursuit* operator|(IVehicleAI* i, AIVehiclePursuitCast_t ext) { return ext(i); }
+
+      //                                //
+      // Verified AIVehicleTraffic cast //
+      //                                //
+
+      struct AIVehicleTrafficCast_t {
+        AIVehicleTraffic* operator()(IVehicleAI* iAI) const {
+          if (!iAI) return nullptr;
+          // Verify cast
+          auto* ai = static_cast<AIVehicleTraffic*>(iAI);
+          if (*reinterpret_cast<std::uintptr_t*>(ai) == static_cast<std::uintptr_t>(0x891CF8)) return ai;
+          // Bad cast
+          return nullptr;
+        }
+      };
+      static AIVehicleTraffic* operator|(IVehicleAI* i, AIVehicleTrafficCast_t ext) { return ext(i); }
+    }  // namespace details
+
+    // Try-get IVehicleAI as AIVehicle
+    // Usage: AIVehicle* myptr = GetAIPtr() | AIVehicleEx::AsAIVehicle;
+    static inline const details::AIVehicleCast_t AsAIVehicle;
+
+    // Try-get IVehicleAI as AIVehicleCopCar
+    // Usage: AIVehicleCopCar* myptr = GetAIPtr() | AIVehicleEx::AsAIVehicleCopCar;
+    static inline const details::AIVehicleCopCarCast_t AsAIVehicleCopCar;
+
+    // Try-get IVehicleAI as AIVehicleEmpty
+    // Usage: AIVehicleEmpty* myptr = GetAIPtr() | AIVehicleEx::AsAIVehicleEmpty;
+    static inline const details::AIVehicleEmptyCast_t AsAIVehicleEmpty;
+
+    // Try-get IVehicleAI as AIVehicleHelicopter
+    // Usage: AIVehicleHelicopter* myptr = GetAIPtr() | AIVehicleEx::AsAIVehicleHelicopter;
+    static inline const details::AIVehicleHelicopterCast_t AsAIVehicleHelicopter;
+
+    // Try-get IVehicleAI as AIVehicleHuman
+    // Usage: AIVehicleHuman* myptr = GetAIPtr() | AIVehicleEx::AsAIVehicleHuman;
+    static inline const details::AIVehicleHumanCast_t AsAIVehicleHuman;
+
+    // Try-get IVehicleAI as AIVehiclePid
+    // Usage: AIVehiclePid* myptr = GetAIPtr() | AIVehicleEx::AsAIVehiclePid;
+    static inline const details::AIVehiclePidCast_t AsAIVehiclePid;
+
+    // Try-get IVehicleAI as AIVehiclePursuit
+    // Usage: AIVehiclePursuit* myptr = GetAIPtr() | AIVehicleEx::AsAIVehiclePursuit;
+    static inline const details::AIVehiclePursuitCast_t AsAIVehiclePursuit;
+
+    // Try-get IVehicleAI as AIVehicleTraffic
+    // Usage: AIVehicleTraffic* myptr = GetAIPtr() | AIVehicleEx::AsAIVehicleTraffic;
+    static inline const details::AIVehicleTrafficCast_t AsAIVehicleTraffic;
+
+    // Get a pointer to the player IRigidBody instance
+    static IVehicleAI* GetPlayerInstance() {
+      auto* pvehicle = PVehicleEx::GetPlayerInstance();
+      if (pvehicle) return pvehicle->GetAIVehiclePtr();
+
+      return nullptr;
+    }
+  }  // namespace AIVehicleEx
+
 }  // namespace OpenSpeed::MW05
