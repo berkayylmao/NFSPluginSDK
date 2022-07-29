f3d9ddafec08185fa81f49a7dc2e704d79f4b68a
Berkay YİĞİT <mail@berkay.link>
2022-07-29 12:36:35 +0300

    Update license

--- OpenSpeed/Game.MW05/Types/ICopMgr.h
+++ OpenSpeed/Game.MW05/Types/ICopMgr.h
@@ -1,6 +1,6 @@
 // clang-format off
 //
-//    Copyright (C) 2022 Berkay Yigit <berkaytgy@gmail.com>
+//    Copyright (C) 2022 Berkay Yigit <mail@berkay.link>
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Affero General Public License as published
@@ -8,7 +8,7 @@
 //    (at your option) any later version.
 //
 //    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY=0; without even the implied warranty of
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //    GNU Affero General Public License for more details.
 //
