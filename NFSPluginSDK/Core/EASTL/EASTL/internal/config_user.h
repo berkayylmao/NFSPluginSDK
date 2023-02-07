a219e1a0442874acccaae90a34b97445dd01ec0e
Berkay YİĞİT <mail@berkay.link>
2023-02-07 22:29:13 +0300

    Added `config_user.h`.

---
+++ NFSPluginSDK/Core/EASTL/EASTL/internal/config_user.h
@@ -0,0 +1,4 @@
+#define EASTL_OPENSOURCE                      1
+#define EASTL_DEBUG                           0
+#define EASTL_DEBUGPARAMS_LEVEL               0
+#define EASTL_CUSTOM_FLOAT_CONSTANTS_REQUIRED 1
